4657155a9b3a77fc307118895ec9df747a385a22
baldonharris <hbaldon93@gmail.com>
2016-05-22 01:59:22 +0800

    Changes Loans to Account Overview

--- application/views/customers/listing.php
+++ application/views/customers/listing.php
@@ -140,7 +140,7 @@
 	<div class="col-md-12 col-sm-12 col-xs-12" style="padding-left: 10px; padding-right: 10px;">
 		<div class="x_panel">
 			<div class="x_title">
-				<h2>Loans</h2>
+				<h2>Account Overview</h2>
 				<ul class="nav navbar-right panel_toolbox">
 					<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
 				</ul>
