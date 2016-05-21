25eb2aa8e486a31a96c56cdc736fb067ae481690
baldonharris <hbaldon93@gmail.com>
2016-05-21 23:57:38 +0800

    Modified and emptied DB

--- assistone.sql
+++ assistone.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2016 at 04:13 AM
+-- Generation Time: May 21, 2016 at 05:56 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -57,24 +57,9 @@ CREATE TABLE IF NOT EXISTS `customers` (
   `registered` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `guarantor_customers_id` int(11) DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
-  `display_picture` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`id`, `customer_id`, `firstname`, `middlename`, `lastname`, `mobilenumber`, `address`, `registered`, `guarantor_customers_id`, `deleted_at`, `display_picture`) VALUES
-(1, '16-0001', 'Myrna', NULL, 'Pao', '999999', 'None', '2016-05-10 02:24:28', NULL, NULL, ''),
-(2, '16-0002', 'Raineria', 'Cabacog', 'Salabsab', '999999', 'Bunga Mar, Jagna, Bohol', '2016-05-10 15:03:52', 1, NULL, ''),
-(3, '16-0003', 'Maria', NULL, 'Sajul', '9999999', 'None', '2016-05-10 15:05:19', NULL, NULL, ''),
-(4, '16-0004', 'Yolly', NULL, 'Estapia', '9999999', 'Bunga Mar, Jagna, Bohol', '2016-05-10 15:05:19', NULL, NULL, ''),
-(5, '16-0005', 'Haydee/G', NULL, 'Baldon', '999999999', 'Bunga Mar, Jagna, Bohol', '2016-05-10 15:10:16', NULL, NULL, ''),
-(6, '16-0006', 'Chocho', NULL, 'Nalasa', '999999', 'Bunga Mar, Jagna, Bohol', '2016-05-10 15:10:16', NULL, NULL, ''),
-(7, '16-0007', 'Haydee/Dakki', NULL, 'Baldon', '99999', 'Bunga Mar, Jagna, Bohol', '2016-05-10 15:10:16', NULL, NULL, ''),
-(8, '16-0008', 'Aning', 'Cabacog', 'Dabatos', '999999', 'Bunga Mar, Jagna, Bohol', '2016-05-10 15:10:16', NULL, NULL, ''),
-(9, '16-0009', 'Jongoy', NULL, 'Tan', '99999', 'Canjulao, Jagna, Bohol', '2016-05-10 15:10:16', NULL, NULL, ''),
-(10, '16-0010', 'Wilfredo', NULL, 'Matunhay', '999999', 'None', '2016-05-11 05:21:13', NULL, NULL, NULL);
+  `display_picture` varchar(45) DEFAULT NULL,
+  `complete_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -105,7 +90,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
