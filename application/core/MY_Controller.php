1fa79a05f85be6c6b7174161e0ada35004a20c06
baldonharris <hbaldon93@gmail.com>
2016-05-24 05:26:07 +0800

    Fixed bug landing page after login

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,6 +9,10 @@ class MY_Controller extends CI_Controller {
 	}
 	
 	public function generate_page($page = NULL, $data = array()){
+		if(!isset($data['page'])) $data['page'] = array('curr_page'=>0);
+		if(!isset($data['set_sortby'])) $data['set_sortby'] = 1;
+		if(!isset($data['set_orderby'])) $data['set_orderby'] = 2;
+		if(!isset($data['set_display'])) $data['set_display'] = 0;
 		if(!isset($data['css'])) $data['css'] = array();
 		if(!isset($data['js'])) $data['js'] = array();
 		if(!isset($data['data'])) $data['data'] = array();
