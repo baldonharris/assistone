3aef234ae6f4d96aeb853aa1d4704f652bba625c
baldonharris <hbaldon93@gmail.com>
2016-07-07 23:27:26 +0800

    Login page fixed

--- application/views/login.php
+++ application/views/login.php
@@ -18,34 +18,36 @@
 		<script src="assets/js/jquery.min.js"></script>
 	</head>
 	<body style="background:#F7F7F7;">
-		<div class="col-md-4 col-md-offset-4 col-xs-12 col-sm-12" style="margin-top:5%">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<h1 style="text-align:center">
-						<img src="<?=base_url('assets/img/assistone/logo.png')?>"/>
-						<br/>
-						assist<span style="color:#fb8200">one</span>
-					</h1>
-					<form class="form-horizontal" id="login_form" baseurl="<?= base_url() ?>" action="<?=base_url('account/login')?>" method="post">
-						<div class="form-group">
-							<div class="col-sm-12">
-								<input type="text" name="username" class="form-control" placeholder="Username" required="" />
+		<div class="container">
+			<div class="col-md-4 col-md-offset-4 col-xs-12 col-sm-12" style="margin-top:5%">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<h1 style="text-align:center">
+							<img src="<?=base_url('assets/img/assistone/logo.png')?>"/>
+							<br/>
+							assist<span style="color:#fb8200">one</span>
+						</h1>
+						<form class="form-horizontal" id="login_form" baseurl="<?= base_url() ?>" action="<?=base_url('account/login')?>" method="post">
+							<div class="form-group">
+								<div class="col-sm-12 col-xs-12">
+									<input type="text" name="username" class="form-control" placeholder="Username" required="" />
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-12">
-								<input type="password" name="password" class="form-control" placeholder="Password" required="" />
+							<div class="form-group">
+								<div class="col-sm-12 col-xs-12">
+									<input type="password" name="password" class="form-control" placeholder="Password" required="" />
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-12">
-								<button type="submit" class="btn btn-default submit btn-block btn-warning">Log in</button>
+							<div class="form-group">
+								<div class="col-sm-12 col-xs-12">
+									<button type="submit" class="btn btn-default submit btn-block btn-warning">Log in</button>
+								</div>
 							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
+				<p style="text-align:center"><small>©2016 All Rights Reserved.</small></p>
 			</div>
-			<p style="text-align:center"><small>©2016 All Rights Reserved.</small></p>
 		</div>
 		<script type="text/javascript" src="<?=base_url()?>assets/js/notify/pnotify.custom.min.js"></script>
 		<script>
@@ -54,7 +56,6 @@
 				$('#login_form').submit(function(event){
 					event.preventDefault();
 					$.post($(this).attr('action'), $(this).serialize(), function(response){
-						console.log(response);
 						if(parseInt(response)){
 							window.location.href = $('#login_form').attr('baseurl');
 						}else{
