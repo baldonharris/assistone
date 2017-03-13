6e149fa4028d0471ddfc43b0daac86723314742e
baldonharris <hbaldon93@gmail.com>
2017-03-13 23:31:21 +0800

    Collection Statement
    Improved algo and added sort for the data

--- application/controllers/Reports.php
+++ application/controllers/Reports.php
@@ -9,6 +9,44 @@ class Reports extends MY_Controller {
         $this->load->model('m_reports');
     }
     
+    private function sort_us($data){
+        function make_comparer() {
+            $criteria = func_get_args();
+            foreach ($criteria as $index => $criterion) {
+                $criteria[$index] = is_array($criterion)
+                    ? array_pad($criterion, 3, null)
+                    : array($criterion, SORT_ASC, null);
+            }
+
+            return function($first, $second) use ($criteria) {
+                foreach ($criteria as $criterion) {
+                    list($column, $sortOrder, $projection) = $criterion;
+                    $sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
+
+                    if ($projection) {
+                        $lhs = call_user_func($projection, $first[$column]);
+                        $rhs = call_user_func($projection, $second[$column]);
+                    }
+                    else {
+                        $lhs = $first[$column];
+                        $rhs = $second[$column];
+                    }
+
+                    if ($lhs < $rhs) {
+                        return -1 * $sortOrder;
+                    }
+                    else if ($lhs > $rhs) {
+                        return 1 * $sortOrder;
+                    }
+                }
+
+                return 0;
+            };
+        }
+        usort($data, make_comparer('customer_name', 'date_of_release'));
+        return $data;
+    }
+    
     public function collection_statement(){
         $id = $this->input->post('id');
         $this->generate_page('reports/collection_statement', [
@@ -21,28 +59,20 @@ class Reports extends MY_Controller {
     
     public function get_collection_statement(){
         $due_date = $this->input->post('due_date');
-        $hey = ($due_date) ? 'p.due_date="'.$due_date.'"' : NULL;
-        $data = $this->m_reports->get_collection_statement( ($due_date) ? 'p.due_date="'.$due_date.'"' : NULL );
+        $data['data'] = $this->m_reports->get_collection_statement( ($due_date) ? 'p.due_date="'.$due_date.'"' : NULL );
         $data['sub_detail']['total_paid_amount'] = 0;
         $data['sub_detail']['total_due_amount'] = 0;
+        
         for($x=0; $x<count($data['data']); $x++){
-            $data['data'][$x]['total_paid_amount'] = 0;
-            for($y=0; $y<count($data['raw_data']); $y++){
-                if($data['data'][$x]['id'] == $data['raw_data'][$y]['loan_id']){
-                    $data['data'][$x]['total_paid_amount'] += $data['raw_data'][$y]['amount_paid'];
-                    if($data['raw_data'][$y]['payment_actual_paid_date'] === $due_date){
-                        $data['data'][$x] = array_merge($data['data'][$x], array('due_amount'=>$data['raw_data'][$y]['payment_due_amount']));
-                        $data['data'][$x] = array_merge($data['data'][$x], array('amount_paid'=>$data['raw_data'][$y]['amount_paid']));
-                        break;
-                    }else{
-                        $data['data'][$x] = array_merge($data['data'][$x], array('due_amount'=>$data['raw_data'][$y]['payment_due_amount']));
-                    }
-                }
-            }
+            $raw_data = $this->m_reports->get_raw_collection_statement(array('p.id'=>$data['data'][$x]['payment_id'], 'p.due_date'=>$due_date));
+            $data['data'][$x]['due_amount'] = $raw_data[0]['payment_due_amount'];
+            $data['data'][$x]['amount_paid'] = $raw_data[0]['amount_paid'];
+            $data['data'][$x]['total_paid_amount'] = $this->m_reports->get_total_paid_amount(array('p.loans_id'=>$data['data'][$x]['id']))[0]['total_amount_paid'];
+            
             $data['sub_detail']['total_paid_amount'] += $data['data'][$x]['amount_paid'];
             $data['sub_detail']['total_due_amount'] += $data['data'][$x]['due_amount'];
         }
-        unset($data['raw_data']);
+        $data['data'] = $this->sort_us($data['data']);
         echo json_encode(array('status'=>1, 'data'=>$data));
     }
 
--- application/models/m_reports.php
+++ application/models/m_reports.php
@@ -5,12 +5,12 @@ class M_Reports extends CI_Model {
     
     public function get_raw_collection_statement($where = NULL){
         $this->db->order_by('p.id', 'ASC');
-        $this->db->select('l.id as loan_id, p.id as payment_id, p.due_amount as payment_due_amount, p.actual_paid_date as payment_actual_paid_date, p.amount_paid');
+        $this->db->select('l.id as loan_id, p.id as payment_id, p.due_amount as payment_due_amount, p.actual_paid_date as payment_actual_paid_date, p.amount_paid, p.due_date');
         $this->db->join('payments as p', 'p.loans_id=l.id', 'left');
         if(!$where){
             return $this->db->get('loans as l')->result_array();
         }else{
-            return $this->db->get('loans as l', $where)->result_array();
+            return $this->db->get_where('loans as l', $where)->result_array();
         }
     }
 
@@ -20,11 +20,20 @@ class M_Reports extends CI_Model {
         $this->db->join('customers as c', 'c.id=l.customer_id', 'left');
         $this->db->join('payments as p', 'p.loans_id=l.id', 'left');
         if(!$where){
-            $data = $this->db->get('loans as l')->result_array();
+            return $this->db->get('loans as l')->result_array();
         }else{
-            $data = $this->db->get_where('loans as l', $where)->result_array();
+            return $this->db->get_where('loans as l', $where)->result_array();
+        };
+    }
+    
+    public function get_total_paid_amount($where = NULL){
+        $this->db->order_by('p.id', 'ASC');
+        $this->db->select('sum(p.amount_paid) as total_amount_paid');
+        if(!$where){
+            return $this->db->get('payments as p')->result_array();
+        }else{
+            return $this->db->get_where('payments as p', $where)->result_array();
         }
-        return array('data'=>$data, 'raw_data'=>$this->get_raw_collection_statement($where));
     }
     
 }
