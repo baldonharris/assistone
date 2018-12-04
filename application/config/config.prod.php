b24a3ab4d5b7854b8db5bd7961df3c50708bcf11
Harris Baldon <hbaldon93@gmail.com>
2018-12-04 14:39:19 +0000

    Sync Production

--- application/config/config.prod.php
+++ application/config/config.prod.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://theassistone.com';
+$config['base_url'] = 'http://www.theassistone.com';
 
 /*
 |--------------------------------------------------------------------------
